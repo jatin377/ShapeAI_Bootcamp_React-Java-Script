fbd0335f1dbdc9442b3d4a43fec450c6b74cac1a
jatin377 <85388603+jatin377@users.noreply.github.com>
2021-06-06 18:23:23 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note"
+
+function App() {
+  return(
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+      <Note />
+      <Note />
+    
+
+      </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note() {
+  return(
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+        <p>This was an amazing bootcamp encouraged me to learn more and more about javascript and react and basic web designing stuff like html an css.A huge thanks to shaurya sinha sir and team.</p>
+  </div> 
+  );
+}
+
+export default Note;
